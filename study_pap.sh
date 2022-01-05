7029702b9a7c9bcd165d03ec37ae53a3629f46ce
sbstndbs <sebastien.dubois4@gmail.com>
2022-01-05 05:55:10 +0100

    Add figure for pap study
    - figures are from the execution of bash study_pap.sh

--- study_pap.sh
+++ study_pap.sh
@@ -2,10 +2,10 @@ rm time_pap.txt
 ((j = 1)) ; 
 ((inc = 1)) ; 
 ((lim = 10)) ; 
-((burn = 0)) ; 
-((burn_lim = 0)) ;
-((iteration = 1)) ; 
-((iteration_lim = 1)) ; 
+((burn = 10000)) ; 
+((burn_lim = 10)) ;
+((iteration = 100000)) ; 
+((iteration_lim = 10)) ; 
 for ((i=1;i<70;i=i+1));
 	do
 		((j = j+ inc)) ;
