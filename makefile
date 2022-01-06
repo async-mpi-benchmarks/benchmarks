cc0bceaf159821dca7e492a03729c6b16d58939c
sbstndbs <sebastien.dubois4@gmail.com>
2022-01-06 02:01:13 +0100

    Add recouvrmeent
    - recouvrement is a benchmark to study the overlapping betzeen MPI transactions - via MPI_Isend, MPI_Irecv - and computation
    - This is for now just the copy of the current tests/send_test.c .
    - MPI_Test can know if an MPI_Transaction is finished or now without stoping the
    	 execution for a long time
    - TODO :
    	- implementation of compute loops
    	- add Isend from proces !=0
    	- saving times in a file

--- makefile
+++ makefile
@@ -1,19 +1,23 @@
 CC = mpicc 
 CFLAGS = -O3 -Wall
 
-all: 1 pap
+all: 1 pap recouvrement
 
 1 : 1.c
 	${CC} ${CFLAGS} -march=native -mtune=native -funroll-loops -finline-functions -fpeel-loops -ftree-vectorize -ftree-loop-vectorize $^ -o $@  -lm -fopenmp
 	
 pap : pap.c
 	${CC} ${CFLAGS} -march=native -mtune=native -funroll-loops -finline-functions -fpeel-loops -ftree-vectorize -ftree-loop-vectorize $^ -o $@  -lm -fopenmp
-	
+
+
+recouvrement : recouvrement.c
+	${CC} ${CFLAGS} -march=native -mtune=native -funroll-loops -finline-functions -fpeel-loops -ftree-vectorize -ftree-loop-vectorize $^ -o $@  -lm -fopenmp
+		
 	
 
 clean :	
-	rm 1 pap
+	rm 1 pap recouvrement
 	
 run : 
-	mpirun ./1 mpirun -np 2 pap
+	mpirun ./1 &&  mpirun -np 2 pap && mpirun -np 2 ./recouvrememt 
 	
---
+++ recouvrement.c
@@ -0,0 +1,125 @@
+///////////////////////////////////////////////////////////////////////////
+// this is a code to test the MPI_Test call. 
+// Syntax : int MPI_Test(MPI_Request *request, int *flag, MPI_Status *status)
+// flag is True if the operation identified by the request is complete
+// protocol : 
+//	- async array Isend from process 0 to 1
+//	- async Irecv in process 1
+//	- compute loop in process 1
+//		- expensive arithmetic operations
+//		- MPI_Test call to test  during computation AND during recv
+//		- save to what tick when flag is set to True
+//	- barrier MPI_Wait to enforce terminaison in this point 
+
+// objective : - Is it possible to determine covering in async MPI with MPI_Test ?
+//	       - What is the cost of an MPI_Test call ?
+//////////////////////////////////////////////////////////////////////////////
+
+
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <mpi.h>
+
+
+inline
+unsigned long long rdtsc(void)
+{
+  unsigned long long a, d;
+ 
+  __asm__ volatile ("rdtsc" : "=a" (a), "=d" (d));
+  
+  return (d << 32) | a;
+}
+
+
+int main(int argc, char** argv){
+
+	int world_rank;
+	int world_size;
+	MPI_Init(&argc,&argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+	
+
+			
+	int size_array = 1000000 ; 
+	float * array = malloc(size_array * sizeof(float)) ; 
+	
+	
+	// initialization of array
+	if (world_rank == 0){
+		for (int i = 0 ; i < size_array ; i++){
+			array[i] = (float)i ;
+		}
+	}
+	else{
+		for (int i = 0 ; i < size_array ; i++){
+			array[i] = 0.0 ;
+		}
+	}
+		
+
+
+
+	
+	unsigned long long start, end_test, end_wait ; 
+	unsigned long long total_wait , total_test ; 	
+	int flag = 0 ; 
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	// in a async way
+	if (world_rank == 0){
+		printf("\nSending array values to processes in a async way ..\n\n") ; 
+		// send value to every other processes
+
+
+		MPI_Request *requests ;
+		requests = malloc( (world_size - 1 ) * sizeof(MPI_Request) ) ;  
+		for (int i = 0 ; i < world_size - 1 ; i++)
+		{
+			requests[i] = MPI_REQUEST_NULL ; 
+		}
+
+
+		MPI_Barrier(MPI_COMM_WORLD);
+		start = rdtsc() ; 	
+		for (int i = 1 ; i < world_size ; i++){
+			MPI_Isend(array, size_array , MPI_FLOAT , i , i , MPI_COMM_WORLD, &requests[i-1]) ;  
+		}
+
+		MPI_Waitall(world_size- 1 , requests , MPI_STATUS_IGNORE) ;  
+		end_wait = rdtsc() ; 
+		total_wait = end_wait - start ; 	
+		printf("%llu time_wait from process 0\n" , total_wait) ;
+		printf("If the two previous times are similars, then the MPI_Test function works fine\n");			
+	}
+	else if (world_rank < world_size){
+		MPI_Request request = MPI_REQUEST_NULL ; 
+		MPI_Barrier(MPI_COMM_WORLD);
+		start = rdtsc() ; 	
+		MPI_Irecv(array , size_array , MPI_FLOAT , 0 , world_rank , MPI_COMM_WORLD , &request ) ;
+		while (flag == 0){
+			MPI_Test(&request , &flag,  MPI_STATUS_IGNORE) ;   
+		}	
+		end_test = rdtsc() ; 	
+		MPI_Wait(&request , MPI_STATUS_IGNORE) ; 
+		end_wait = rdtsc() ; 	
+		
+		total_test = end_test - start ;		
+		total_wait = end_wait - start ; 	
+		printf("%llu time test from process %d\n" , total_test, world_rank) ;
+		printf("%llu time_wait from process %d\n" , total_wait, world_rank) ;
+		
+		
+	}		
+		
+	
+	MPI_Finalize();
+	
+}	
+
+
+
+
