0f58fc498630789a953b70bb3721f325b8d01f59
sbstndbs <sebastien.dubois4@gmail.com>
2022-01-06 03:25:06 +0100

    Add timers

--- recouvrement.c
+++ recouvrement.c
@@ -43,7 +43,9 @@ int main(int argc, char** argv){
 	
 
 			
-	int size_array = 1000000 ; 
+	int size_array = 1000 ; 
+	unsigned long long  loop_iteration = 100 ; 
+	unsigned long long  loop_max_iteration = 1000000000 ; 
 	float * array = malloc(size_array * sizeof(float)) ; 
 	
 	
@@ -63,8 +65,8 @@ int main(int argc, char** argv){
 
 
 	
-	unsigned long long start, end_test, end_wait ; 
-	unsigned long long total_wait , total_test ; 	
+	unsigned long long start, end_test, end_wait, end_last_loop; 
+	unsigned long long time_compute , time_mpi ; 
 	int flag = 0 ; 
 
 	MPI_Barrier(MPI_COMM_WORLD);
@@ -84,35 +86,78 @@ int main(int argc, char** argv){
 
 
 		MPI_Barrier(MPI_COMM_WORLD);
-		start = rdtsc() ; 	
+		//start = rdtsc() ; 	
 		for (int i = 1 ; i < world_size ; i++){
 			MPI_Isend(array, size_array , MPI_FLOAT , i , i , MPI_COMM_WORLD, &requests[i-1]) ;  
 		}
 
 		MPI_Waitall(world_size- 1 , requests , MPI_STATUS_IGNORE) ;  
-		end_wait = rdtsc() ; 
-		total_wait = end_wait - start ; 	
-		printf("%llu time_wait from process 0\n" , total_wait) ;
-		printf("If the two previous times are similars, then the MPI_Test function works fine\n");			
+		//end_wait = rdtsc() ; 
+		//total_wait = end_wait - start ; 	
+		//printf("%llu time_wait from process 0\n" , total_wait) ;
+		//printf("If the two times for a specific process are similars, then the MPI_Test function works fine\n");	
+		
+		MPI_Barrier(MPI_COMM_WORLD);		
+				
 	}
 	else if (world_rank < world_size){
 		MPI_Request request = MPI_REQUEST_NULL ; 
+		unsigned long long i = 0 ; 
+		unsigned long long i2 = 0 ; 
+		double compute = 0.0 ; 
 		MPI_Barrier(MPI_COMM_WORLD);
+		
 		start = rdtsc() ; 	
 		MPI_Irecv(array , size_array , MPI_FLOAT , 0 , world_rank , MPI_COMM_WORLD , &request ) ;
-		while (flag == 0){
+		
+		/////////////////////////////////////////////////////////////////
+		// compute & tests many times. 
+		// exit : 
+		//	- if loop made more than loop_max_iteration additions. 
+		//		thus, end_test is the computation time
+		//	or
+		//	- if the MPI transaction is finished.
+		//		thus, end_test is the MPI transaction time, and
+		//		computation have to continue in a normal time
+		/////////////////////////////////////////////////////////////////
+		
+		while ( (flag == 0) || (i < loop_max_iteration)  ){
+			for (unsigned long long  j = 0 ; j < loop_iteration ; j++){
+				compute += 0.054398 ; // float operation
+				i += 1 ; 	      // int operation
+			}
 			MPI_Test(&request , &flag,  MPI_STATUS_IGNORE) ;   
 		}	
-		end_test = rdtsc() ; 	
+		end_test = rdtsc() ;
 		MPI_Wait(&request , MPI_STATUS_IGNORE) ; 
-		end_wait = rdtsc() ; 	
+		end_wait = rdtsc() ; 
+		i2 = i ; 	
+		while (i < loop_max_iteration){
+			for (int j = 0 ; j < loop_iteration ; j++){
+				compute += 0.054398 ; // float operation
+				i += 1 ; 	// int operation
+			}  
+		}			
 		
-		total_test = end_test - start ;		
-		total_wait = end_wait - start ; 	
-		printf("%llu time test from process %d\n" , total_test, world_rank) ;
-		printf("%llu time_wait from process %d\n" , total_wait, world_rank) ;
+		end_last_loop = rdtsc() ;		
 		
+		if (i2 >= loop_max_iteration){
+			// first loop ended due to loop_max_iteration
+			time_compute = end_test - start ; 
+			time_mpi = end_wait - start ;
+			printf("computation faster\n");
+		}
+		else{
+			// first loop ended du to finished MPI transaction
+			time_compute = end_last_loop - start ; 
+			time_mpi = end_wait - start ; 
+			printf("MPI transaction faster\n");
+		}
+		printf("computed values from process %d are %llu and %f\n" , world_rank , i, compute) ;		 		
+		printf("%llu time_compute from process %d\n" , time_compute, world_rank) ;
+		printf("%llu time_mpi     from process %d\n" , time_mpi, world_rank) ;
 		
+		MPI_Barrier(MPI_COMM_WORLD);		
 	}		
 		
 	
